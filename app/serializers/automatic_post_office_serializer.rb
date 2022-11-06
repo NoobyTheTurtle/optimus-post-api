54b9e233ab7c69bc1fe05913d24385df72c683ce
noobytheturtle <noobytheturtle@air-aleksej.ru>
2022-11-06 17:49:09 +0300

    Remove some files

--- app/serializers/automatic_post_office_serializer.rb
+++ app/serializers/automatic_post_office_serializer.rb
@@ -1,5 +1,5 @@
 class AutomaticPostOfficeSerializer < ActiveModelSerializer
-  attributes :geo_data, :area_id, :area_name, :is_placed, :address, :placement_object_type_id, :people_in_range,
+  attributes :geo_data, :area_id, :area_name, :district_id, :is_placed, :address, :placement_object_type_id, :people_in_range,
              :distance_to_metro, :distance_to_bus, :predict_a, :predict_b, :predict_c
 
   def area_name
--- db/migrate/20221023072556_create_districts.rb
+++ db/migrate/20221023072556_create_districts.rb
@@ -8,5 +8,22 @@ class CreateDistricts < ActiveRecord::Migration[7.0]
 
       t.timestamps
     end
+    districts_data.each do |data|
+      District.find_or_create_by(data)
+    end
+  end
+
+  def districts_data
+    [
+      { id: 1, short_name: 'ЦАО', name: 'Центральный', square: 66.18, population: 779_086 },
+      { id: 2, short_name: 'САО', name: 'Северный', square: 113.73, population: 1_175_229 },
+      { id: 3, short_name: 'СВАО', name: 'Северо-Восточный', square: 101.88, population: 1_427_597 },
+      { id: 4, short_name: 'ВАО', name: 'Восточный', square: 154.84, population: 1_514_420 },
+      { id: 5, short_name: 'ЮВАО', name: 'Юго-Восточный', square: 117.56, population: 1_432_839 },
+      { id: 6, short_name: 'ЮАО', name: 'Южный', square: 131.77, population: 1_773_425 },
+      { id: 7, short_name: 'ЮЗАО', name: 'Юго-Западный', square: 111.36, population: 1_442_971 },
+      { id: 8, short_name: 'ЗАО', name: 'Западный', square: 153.03, population: 1_383_853 },
+      { id: 9, short_name: 'СЗАО', name: 'Северо-Западный', square: 93.28, population: 1_009_217 }
+    ]
   end
 end
